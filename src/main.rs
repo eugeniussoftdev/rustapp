a467b9ab0974d13549fb00da1637282c69ddd892
Eugene <macbook@MacBook-Pro-mac.local>
2023-11-27 16:11:12 +0200

    add plus minus functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
 use yew::prelude::*;
 
+struct Model {
+    value: i64,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    let state = use_state(|| Model { value: 0 });
+
+    let plusStateHandler = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1,
+            })
+        })
+    };
+
+    let minusStateHnadler = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value - 1,
+            })
+        })
+    };
+
     html! {
         <div>
-            <button>{ "Press" }</button>
+        <p>{ state.value} </p>
+            <button onclick={plusStateHandler} >{ "Plus" }</button>
+            <button onclick={minusStateHnadler} >{ "Minus" }</button>
         </div>
     }
 }
