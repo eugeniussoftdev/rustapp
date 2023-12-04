6e36c9961f7ecb144f0f542610665f218c0bd1ca
Eugene <9867849+eugeniussoftdev@users.noreply.github.com>
2023-12-04 20:57:35 +0000

    add onchange handlers (#8)
    Co-authored-by: Eugene <macbook@MacBook-Pro-mac.local>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use std::fmt::format;
+use std::default;
 
+use log::info;
+use log::log;
+use wasm_bindgen::JsCast;
+use wasm_bindgen::JsValue;
+use web_sys::{EventTarget, HtmlInputElement};
 use yew::prelude::*;
 
 #[derive(Clone, Properties, PartialEq)]
@@ -40,6 +45,11 @@ fn app() -> Html {
         }],
     });
 
+    let inputField = use_state(|| String::new());
+
+    let input_value_handle = use_state(String::default);
+    let input_value = (*input_value_handle).clone();
+
     let plusStateHandler = {
         let state = state.clone();
 
@@ -64,11 +74,13 @@ fn app() -> Html {
 
     let addPersonToList = {
         let state = state.clone();
+        let newName = input_value.clone();
 
         Callback::from(move |_| {
+            let newName = newName.clone();
             let newItem = ListItem {
                 id: state.list.len() as i32 + 1,
-                name: format!("Name {}", state.list.len() + 1),
+                name: newName,
                 phone: 5555555,
             };
 
@@ -80,7 +92,41 @@ fn app() -> Html {
                 list: newList,
             })
         })
+        // .into()
     };
+
+    let addNewPersonName = {
+        let inputField = inputField.clone();
+
+        Callback::from({
+            let inputField = inputField.clone();
+            move |input_event: InputEvent| {
+                let target: HtmlInputElement =
+                    input_event.current_target().unwrap().dyn_into().unwrap();
+
+                inputField.set(target.value());
+            }
+        })
+        // .into()
+    };
+
+    let on_cautious_change = {
+        let input_value_handle = input_value_handle.clone();
+
+        Callback::from(move |e: Event| {
+            let input = e.target_dyn_into::<HtmlInputElement>();
+
+            if let Some(input) = input {
+                input_value_handle.set(input.value());
+            }
+        })
+    };
+
+    let on_dangerous_change = Callback::from(move |e: Event| {
+        // You must KNOW target is a HtmlInputElement, otherwise
+        // the call to value would be Undefined Behaviour (UB).
+        input_value_handle.set(e.target_unchecked_into::<HtmlInputElement>().value());
+    });
 
     html! {
         <div>
@@ -88,7 +134,30 @@ fn app() -> Html {
             <button onclick={plusStateHandler} >{ "Plus" }</button>
             <button onclick={minusStateHnadler} >{ "Minus" }</button>
             <div>
-            <button onclick={addPersonToList}>{ "Add New Person" }</button>
+            <>
+            <label for="cautious-input">
+                { "My cautious input:" }
+                <input onchange={on_cautious_change}
+                    id="cautious-input"
+                    type="text"
+                    value={input_value.clone()}
+                />
+            </label>
+            <label for="dangerous-input">
+                { "My dangerous input:" }
+                <input onchange={on_dangerous_change}
+                    id="dangerous-input"
+                    type="text"
+                    value={input_value}
+                />
+            </label>
+        </>
+            <div style="display: flex; flex-direction: column; width: 300px; margin-top: 20px;">
+            <p>{&*inputField} </p>
+                // <input type="text" placeholder="add new user" style="height: 50px" onchange={addNewPersonName} value={(*inputField).clone()} />
+                <button style="height: 50px" onclick={addPersonToList}>{ "Add New Person" }</button>
+            </div>
+
                 <ListComponent list={state.list.clone()} />
             </div>
         </div>
